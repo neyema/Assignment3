7d02380514b32fda3635b89e9a39b714ce83f6e2
tomerme2 <tomer.laor1@gmail.com>
2019-05-26 11:34:01 -0700

    fixed some x87 stuff

--- drone.s
+++ drone.s
@@ -1,6 +1,6 @@
 ;Generates rand number between 1 and max int
 generate_rand:
-  mov eax, [randStartState]   ;eax is lfsr
+  mov eax, [seed]   ;eax is lfsr
   mov ebx, 0    ;bit will be in bx
   mov esi, 0    ;period will be in esi
   .doLoop:
@@ -24,14 +24,20 @@ generate_rand:
   or ecx, edx
   mov ecx, eax
   add esi, 1
-  cmp eax, [randStartState]
+  cmp eax, [seed]
   jne .doLoop
   push esi ;push period to return it
   ret
 
-;returns 0 or 1 in the stack
+;returns dword 0 or 1 in the stack
 mayDestroy:
   ;needs Y, X and alpha somehow
+  ;assumes pointer to alpha is in eax
+  ;assumes pointer to X is in ebx
+  ;assumes pointer to Y is in ecx
+  ;assumes pointer to X of target in edx
+  ;assumes pointer to Y of target in esi
+
   ret
 
 drone_routine: ;the code for drone co-routine
@@ -43,9 +49,15 @@ drone_routine: ;the code for drone co-routine
   popad
   finit
   fild dword [dronesRandRetHelper]  ;the angle itself
-  fidiv st0, 2147483647   ;max int
-  fmul 120          ;to get [0, 120]
-  fsub 60           ;to get [-60,60]
+  push 2147483647   ;max int
+  fidiv [esp]
+  pop eax
+  push 120
+  fimul [esp]          ;to get [0, 120]
+  pop eax
+  push 60
+  fisub [esp]           ;to get [-60,60]
+  pop eax
   ;pop value into dronesRandAngleF
   fstp [dronesRandAngleF]
   pushad
@@ -54,35 +66,47 @@ drone_routine: ;the code for drone co-routine
   pop dword [dronesRandRetHelper]
   popfd
   popad
-  finit
   fild dword [dronesRandRetHelper]  ;the angle itself
-  fidiv st0, 2147483647   ;max int
-  fmul 50          ;to get [0, 120]
+  push 2147483647   ;max int
+  fidiv [esp]
+  pop eax
+  push 50
+  fmul [esp]          ;to get [0, 50]
+  pop eax
   ;pop value into dronesRandDistance
   fstp [dronesRandDistance]
   ;TODO: get alpha to eax
-  ;assumes that pointer to alpha is on eax for now
-  fild dronesRandAngleF
-  fadd st0, [eax]  ;should be tenword or something like that near eax
-  fild 360
-  ucomiss [eax], st0   ;cmp eax and 360
+  ;assumes that pointer to old alpha is on eax for now
+  fild [dronesRandAngleF]
+  fadd dword [eax]
+  push 360
+  fild [esi]
+  pop esi    ;clean stack
+  ucomiss st1, st0   ;cmp eax and 360
   jb biggerThan360
+  fstp esi    ;clean stack
   fldz    ;loads +0.0
-  ucomiss [eax], st0
+  ucomiss st1, st0
   ja lowerThan0
   jmp angleIsCool
   .biggerThan360:
-    sub eax, 360   ;TODO: TURN TO FPOINT
+    push 360
+    fstp esi    ;clean stack
+    fisub [esi]    ;minus 360
+    pop esi
     jmp .angleIsCool
   .lowerThan0:
-    add eax, 360  ;TODO: TURN TO FPOINT
+    push 0
+    fstp esi    ;clean stack
+    fiadd [esi]    ;plus 360
+    pop esi
     jmp .angleIsCool
   .angleIsCool:
+    fstp [dronesRandAngleF]   ;new alpha in this label
     ;mov [dronesRandAngleF] into alpha (update it)
     ;calculate dx and dy
     ;assumes that pointer to x is in ebx
     ;assumes that pointer to y is in ecx
-    finit
     fild [dronesRandAngleF]
     fcos st0
     fild [dronesRandDistance]
@@ -98,6 +122,7 @@ drone_routine: ;the code for drone co-routine
     fild [ecx]
     fadd st0, st1    ;new_y=dy+old_y
     ;fstp [y]     ;TODO: UCOMMENT IT. updates y!
+    ;TODO: fix overflow/undeflow X/Y
   pushad
   pushfd
   call mayDestroy
